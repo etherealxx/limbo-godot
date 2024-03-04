b97dd832140648ff8f53035ea795aa9ae05c2080
Etherealxx <gwathon3@gmail.com>
2024-03-04 07:33:00 +0700

    messy scrambling part

---
+++ scripts/resources/nextmoveanddelay.gd
@@ -0,0 +1,20 @@
+extends Resource
+class_name NextMoveAndDelay
+
+var nextpos : Vector2i
+var delay : float
+var empty : bool = false
+
+func _init(	_nextpos : Vector2i = Vector2i.ZERO,
+			_delay : float = 0.0,
+			_empty : bool = false):
+				
+	nextpos = _nextpos
+	delay = _delay
+	empty = _empty
+
+func isempty():
+	return empty
+#func setup(_nextpos : Vector2i, _delay : float):
+	#nextpos = _nextpos
+	#delay = _delay
